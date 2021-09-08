d67e925427e1bcade1e363ca79ab107070e6f0b9
unknown <leonardoalmeida2703@gmail.com>
2021-09-08 13:47:01 -0300

    initial commit

---
+++ Spring Boot/LojaDeGames/LojaDeGames/src/test/java/com/lojadegames/LojaDeGames/LojaDeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojadegames.LojaDeGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
