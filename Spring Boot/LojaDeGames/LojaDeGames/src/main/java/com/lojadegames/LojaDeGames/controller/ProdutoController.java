4b21d840959e751ca345d433975dcbd195a71a26
unknown <leonardoalmeida2703@gmail.com>
2021-09-08 16:35:02 -0300

    CRUD do PostagemModel

--- Spring Boot/LojaDeGames/LojaDeGames/src/main/java/com/lojadegames/LojaDeGames/controller/ProdutoController.java
+++ Spring Boot/LojaDeGames/LojaDeGames/src/main/java/com/lojadegames/LojaDeGames/controller/ProdutoController.java
@@ -1,9 +1,21 @@
 package com.lojadegames.LojaDeGames.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lojadegames.LojaDeGames.model.ProdutoModel;
 import com.lojadegames.LojaDeGames.repository.ProdutoRepository;
 
 @RestController
@@ -11,5 +23,50 @@ import com.lojadegames.LojaDeGames.repository.ProdutoRepository;
 public class ProdutoController {
 	private @Autowired ProdutoRepository repository;
 	
+	//GET
+	@GetMapping("/getAll")
+	public ResponseEntity<List<ProdutoModel>> getAllProduto(){
+		List<ProdutoModel> objetoProduto = repository.findAll();
+		
+		if (objetoProduto.isEmpty()) {
+			return ResponseEntity.status(204).build();
+		} else {
+			return ResponseEntity.status(200).body(objetoProduto);
+		}
+	}
 	
+	@GetMapping("/getById/{id_produto")
+	public ResponseEntity<ProdutoModel> getById(@PathVariable(value = "id_produto") Long idProduto){
+		Optional<ProdutoModel> objetoProduto = repository.findById(idProduto);
+		
+		if (objetoProduto.isPresent()) {
+			return ResponseEntity.status(200).body(objetoProduto.get());
+		} else {
+			return ResponseEntity.status(204).build();
+		}
+	}
+
+	@GetMapping("/getByTitulo/{titulo}")
+	public ResponseEntity<ProdutoModel> getByTitulo(@PathVariable(value = "titulo") String titulo) {
+		return repository.findByTitulo(titulo)
+				.map(tituloencontrado -> ResponseEntity.ok(tituloencontrado))
+				.orElse(ResponseEntity.notFound().build());
+	}
+
+	//POST
+	@PostMapping("/criar")
+	public ResponseEntity<ProdutoModel> cadastar(@Valid @RequestBody ProdutoModel novoProduto) {
+		return ResponseEntity.status(201).body(repository.save(novoProduto));
+	}
+
+	//PUT
+	public ResponseEntity<ProdutoModel> atualizar(@Valid @RequestBody ProdutoModel atualizadoProduto) {
+		return ResponseEntity.status(201).body(repository.save(atualizadoProduto));
+	}
+
+	//DELETE
+	@DeleteMapping("deletar/{id_produto}")
+	public void deletar(@PathVariable(value = "id_produto") Long idProduto) {
+		repository.deleteById(idProduto);
+	}
 }
--- Spring Boot/LojaDeGames/LojaDeGames/src/main/java/com/lojadegames/LojaDeGames/repository/ProdutoRepository.java
+++ Spring Boot/LojaDeGames/LojaDeGames/src/main/java/com/lojadegames/LojaDeGames/repository/ProdutoRepository.java
@@ -1,5 +1,7 @@
 package com.lojadegames.LojaDeGames.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.lojadegames.LojaDeGames.model.ProdutoModel;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long>{
-
+	Optional<ProdutoModel> findByTitulo(String titulo);
 }
